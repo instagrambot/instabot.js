9e8378827a59bc7f8c138b2345177adc5ae1c90d
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-01-24 21:46:58 +0300

    configure react build with poi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'airbnb',
   env: {
+    browser: true,
     jasmine: true
   }
 };
---
+++ poi.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+module.exports = {
+  presets: [
+    require('poi-preset-react')(),
+  ],
+};
