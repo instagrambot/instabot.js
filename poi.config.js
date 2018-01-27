4ee10300d5d4c560565957100f19a3644e46dddd
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-01-27 21:26:13 +0300

    now test uses same poi build process

--- poi.config.js
+++ poi.config.js
@@ -1,8 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  entry: 'src/index.jsx',
+  sourceMap: isProd ? false : 'source-map',
+  homepage: '',
+  karma: {
+    frameworks: ['jasmine'],
+    browsers: ['ChromeHeadless'],
+    reporters: ['spec', 'kjhtml'],
+    proxies: {},
+  },
   presets: [
     require('poi-preset-react')(),
+    require('poi-preset-karma')({
+      files: ['./karma.js'],
+    }),
   ],
 };
