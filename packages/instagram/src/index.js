ff655866c80696d1c79ee7b4ccb8ce041b70ac82
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-03-04 16:33:58 +0300

    follow & unfollow methods added

--- packages/instagram/src/index.js
+++ packages/instagram/src/index.js
@@ -18,4 +18,14 @@ module.exports = class Instagram {
     const resp = await this.http.get(`/${name}?__a=1`)
     return resp.body
   }
+
+  async follow (userId) {
+    const resp = await this.http.post(`/web/friendships/${userId}/follow/`)
+    return resp.body
+  }
+
+  async unfollow (userId) {
+    const resp = await this.http.post(`/web/friendships/${userId}/unfollow/`)
+    return resp.body
+  }
 }
