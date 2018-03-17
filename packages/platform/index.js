cb50b0fa42361973aed9d3b8027ae7beda84e22e
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-03-18 01:18:41 +0300

    remove storage

--- packages/platform/index.js
+++ packages/platform/index.js
@@ -1,11 +1,2 @@
-const Storage = require('./storage');
-
 module.exports = class Platform {
-  constructor() {
-    this.settings = new Storage({ file: 'settings.json' });
-  }
-
-  async init() {
-    await this.settings.sync();
-  }
 };
