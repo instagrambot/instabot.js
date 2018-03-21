369bea2fa2af74962f0b241ceb3a305c1ce8d683
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-03-21 22:04:35 +0300

    remove ui package

--- packages/ui/src/layout/Layout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ children }) => (
-  <div className="b-layout">
-    { children }
-  </div>
-);
--- packages/ui/src/layout/Root.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './Layout';
-
-export default () => (
-  <Layout>
-    <b>Hello Instabot.js</b>
-  </Layout>
-);
