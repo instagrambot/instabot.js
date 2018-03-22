c1bc2534b73c4dd3543d3ef6ef5d044cdc023b91
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-03-22 23:35:08 +0300

    minor emotion fixes

--- packages/desktop/src/index.js
+++ packages/desktop/src/index.js
@@ -1,8 +1,28 @@
-import 'normalize.css';
+import { injectGlobal } from 'emotion';
 import React from 'react';
 import { render } from 'react-dom';
 import Instabot from './components/Instabot';
 
-import './index.css';
+injectGlobal(`
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+`);
 
 render(<Instabot />, document.getElementById('app'));
