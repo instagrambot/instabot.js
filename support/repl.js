afc351aef0073ae1ebceb59306c230fb46c2ef18
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-04-01 14:44:10 +0300

    await handle tools

--- support/repl.js
+++ support/repl.js
@@ -1,7 +1,24 @@
+/* eslint-disable no-console */
+
 import WebApi from '@/lib/web-api';
 
+let lastAwait = null;
+
 if (process.env.NODE_ENV !== 'production') {
-  window.$instabot = {
+  Object.defineProperty(window, '$await', {
+    set: (promise) => {
+      promise.then((result) => {
+        lastAwait = result;
+        console.log(lastAwait);
+      });
+
+      promise.catch(err => console.error(err));
+    },
+
+    get: () => lastAwait,
+  });
+
+  window.$repl = {
     api: new WebApi(),
   };
 }
