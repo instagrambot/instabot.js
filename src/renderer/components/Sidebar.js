633f8747f57c1a786ad712b749921beee15545c4
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-04-03 00:22:01 +0300

    minor style fixes

--- src/renderer/components/Sidebar.js
+++ src/renderer/components/Sidebar.js
@@ -3,6 +3,12 @@ import User from './User';
 
 export default () => (
   <div className="b-sidebar">
-    <User />
+    <div className="b-sidebar__header" />
+
+    <div className="b-sidebar__footer">
+      <div className="b-sidebar__item">
+        <User />
+      </div>
+    </div>
   </div>
 );
