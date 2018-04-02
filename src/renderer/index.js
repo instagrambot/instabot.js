b929ea4ec040f250ac7817368d29ee04c8447e66
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-04-02 23:44:47 +0300

    disable zoom and user-select

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import '@/styles/index.scss';
 
+import { webFrame } from 'electron';
 import React from 'react';
 import { render } from 'react-dom';
 import Instabot from './components/Instabot';
 import '../../support/repl';
+
+webFrame.setVisualZoomLevelLimits(1, 1);
+webFrame.setLayoutZoomLevelLimits(1, 1);
 
 render(<Instabot />, document.getElementById('app'));
