ed730913350650cba27a644b7606173796cbf74b
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-04-07 00:15:43 +0300

    minor style fixes

--- src/renderer/components/Flipper.js
+++ src/renderer/components/Flipper.js
@@ -1,33 +1,60 @@
+/* eslint-disable react/forbid-prop-types */
+
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import cn from 'classnames';
 
 export default class Flipper extends Component {
+  static propTypes = {
+    back: propTypes.any,
+    front: propTypes.any,
+  }
+
+  static defaultProps = {
+    back: null,
+    front: null,
+  }
+
   constructor(props) {
     super(props);
 
-    this.handleClick = this.handleClick.bind(this);
     this.state = { active: false };
   }
 
-  handleClick() {
-    this.setState({ active: !this.state.active });
+  toggle = (value) => {
+    this.setState({ active: !!value });
+  }
+
+  renderFront() {
+    const { toggle } = this;
+    const { front } = this.props;
+
+    return (
+      <div className="b-flipper__section b-flipper__section--front">
+        { typeof front === 'function' ? front(toggle) : front }
+      </div>
+    );
+  }
+
+  renderBack() {
+    const { toggle } = this;
+    const { back } = this.props;
+
+    return (
+      <div className="b-flipper__section b-flipper__section--back">
+        { typeof back === 'function' ? back(toggle) : back }
+      </div>
+    );
   }
 
   render() {
     const { state } = this;
+    const classNames = cn('b-flipper', { 'b-flipper--active': state.active });
 
     return (
-      <div
-        className={cn('b-flipper', { 'b-flipper--active': state.active })}
-        onClick={this.handleClick}
-      >
-        <div className="b-flipper__section b-flipper__section--front">
-          Front
-        </div>
-
-        <div className="b-flipper__section b-flipper__section--back">
-          Back
-        </div>
+      <div className={classNames}>
+        { this.renderFront() }
+        { this.renderBack() }
       </div>
     );
   }
