ad8fa1b1fa90170ea96ad83c15f8e4c7b7b38bee
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-05-02 23:45:48 +0300

    validation added for accounts

--- app/components/Control.js
+++ app/components/Control.js
@@ -1,15 +1,29 @@
 import React, { Component, createRef } from 'react';
+import { noop, capitalize } from 'lodash';
 import Types from 'prop-types';
 import cn from 'classnames';
 
 export default class Control extends Component {
   static propTypes = {
-    label: Types.string.isRequired,
+    label: Types.string,
+    name: Types.string,
     type: Types.string,
+    value: Types.string,
+    error: Types.string,
+    onBlur: Types.func,
+    onFocus: Types.func,
+    onChange: Types.func,
   }
 
   static defaultProps = {
     type: 'string',
+    label: '',
+    name: '',
+    value: '',
+    error: '',
+    onBlur: noop,
+    onFocus: noop,
+    onChange: noop,
   }
 
   state = {
@@ -27,38 +41,62 @@ export default class Control extends Component {
     this.setState({ filled: this.inputEl.current.value !== '' });
   }
 
-  handleFocus = () => {
+  handleFocus = (event) => {
+    this.props.onFocus(event);
     this.setState({ focus: true });
   }
 
-  handleBlur = () => {
+  handleBlur = (event) => {
+    this.props.onBlur(event);
     this.setState({ focus: false });
   }
 
+  handleChange = (event) => {
+    this.provideEmpty();
+    this.props.onChange(event);
+  }
+
   render() {
-    const { label, type } = this.props;
+    const {
+      label,
+      type,
+      value,
+      name,
+      error,
+    } = this.props;
+
     const { focus, filled } = this.state;
+    const { handleBlur, handleChange, handleFocus } = this;
 
     const classes = cn('b-control', {
       'b-control--focus': focus,
       'b-control--filled': filled,
+      'b-control--error': error,
     });
 
     return (
       <div className={classes}>
-        <div className="b-control__label">
-          { label }
+        <div className="b-control__body">
+          <div className="b-control__label">
+            { capitalize(label || name) }
+          </div>
+
+          <input
+            ref={this.inputEl}
+            className="b-control__input"
+            name={name}
+            type={type}
+            value={value}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onKeyPress={handleChange}
+            onChange={handleChange}
+          />
         </div>
 
-        <input
-          className="b-control__input"
-          type={type}
-          ref={this.inputEl}
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-          onKeyPress={this.provideEmpty}
-          onChange={this.provideEmpty}
-        />
+        <div className="b-control__hint">
+          &nbsp;{ error }
+        </div>
       </div>
     );
   }
--- app/modules/Accounts/AccountsCreate.js
+++ app/modules/Accounts/AccountsCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Types from 'prop-types';
 import { noop } from 'lodash';
+import { Formik } from 'formik';
+import Yup from 'yup';
 import Flip from '@/components/Flip';
 import Control from '@/components/Control';
 
@@ -13,8 +15,18 @@ export default class AccountsCreate extends Component {
     onBack: noop,
   }
 
+  onSubmit = (e) => {
+    console.log('onSubmit', e);
+  }
+
+  schema = Yup.object().shape({
+    account: Yup.string().required('Required'),
+    password: Yup.string().required('Required'),
+  })
+
   render() {
     const { onBack } = this.props;
+    const { onSubmit, schema } = this;
 
     return (
       <Flip
@@ -22,8 +34,34 @@ export default class AccountsCreate extends Component {
         label="Add account"
         onLabelClick={onBack}
       >
-        <Control label="Account" />
-        <Control label="Password" type="password" />
+        <Formik onSubmit={onSubmit} validationSchema={schema}>
+          {f => (
+            <form className="b-accounts-create__form" onSubmit={f.handleSubmit}>
+              <div className="b-accounts-create__body">
+                <Control
+                  name="account"
+                  value={f.values.account}
+                  onBlur={f.handleBlur}
+                  onChange={f.handleChange}
+                  error={f.touched.account && f.errors.account}
+                />
+
+                <Control
+                  name="password"
+                  type="password"
+                  value={f.values.password}
+                  onBlur={f.handleBlur}
+                  onChange={f.handleChange}
+                  error={f.touched.password && f.errors.password}
+                />
+              </div>
+
+              <div className="b-accounts-create__footer">
+                <button className="b-btn b-btn--block">Submit</button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </Flip>
     );
   }
