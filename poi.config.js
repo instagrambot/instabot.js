46218866a533b034444de626d3b3c3e7f03df868
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-15 15:13:29 +0300

    poi config added

---
+++ poi.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { resolve } = require('path');
+const fromPairs = require('lodash/fromPairs');
+const pkg = require('./package.json');
+
+const isProd = process.env.NODE_ENV === 'production';
+const externals = pkg.externalDependencies.map(v => [v, `require('electron').remote.require("${v}")`]);
+
+module.exports = {
+  title: 'Instabot',
+  publicPath: './',
+  sourceMap: isProd ? false : 'eval-source-map',
+
+  extendWebpack(config) {
+    config.target('electron-renderer');
+    config.externals(fromPairs(externals));
+
+    config.node
+      .merge({ __dirname: false });
+
+    config.plugins
+      .delete('split-vendor-code')
+      .delete('split-manifest');
+
+    config.module
+      .rule('js')
+      .exclude.add(/node_modules/);
+
+    config.resolve.alias
+      .set('@', resolve('app'));
+  },
+};
