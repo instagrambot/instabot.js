6afda5aea5af4883ee17c32fd370b2d156e03dc7
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-17 12:30:29 +0300

    Icon property added

---
+++ app/components/Icon.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+
+interface IIconProps {
+  name: string;
+}
+
+export default class extends Component<IIconProps, {}> {
+  static displayName = 'Icon';
+
+  render() {
+    return (
+      <i className={`ion-${this.props.name} b-icon`} />
+    );
+  }
+}
